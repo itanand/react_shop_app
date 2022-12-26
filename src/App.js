57e35726a400903b302b6d6c068cbe2f45decc17
Anand Mohan <anand1505mohan@gmail.com>
2022-12-26 11:44:38 +0530

    Navbar Item

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {
   createBrowserRouter,
   Outlet
 } from "react-router-dom";
+
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
+import "./App.scss";
 
 const Layout = () => {
   return(
