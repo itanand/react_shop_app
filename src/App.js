32b4ece12628af935f47f88472461fa20c86b2fe
Anand Mohan <anand1505mohan@gmail.com>
2022-12-26 10:35:13 +0530

    React-shop-app- react-router-dom-basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Outlet
+} from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+
+const Layout = () => {
+  return(
+    <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter ([
+{
+  path: "/",
+  element: <Home />
+},
+{
+  path: "/products/:id",
+  element: <Products />
+},
+{
+  path: "/product/:id",
+  element: <Product />
+}
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
