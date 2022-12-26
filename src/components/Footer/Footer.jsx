467a229a9f8d61e57aefbf2f63b004bf1bf675ef
Anand Mohan <anand1505mohan@gmail.com>
2022-12-26 12:10:10 +0530

    Footer-Work-completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Bags</span>
+          <span>Accessories</span>
+          <span>Electronics</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+        <h1>About</h1>
+           <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit magni
+             earum laboriosam excepturi pariatur expedita eum laudantium voluptatibus amet
+             </span>
+        </div>
+        <div className="item">
+        <h1>Contact</h1>
+           <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit magni
+             earum laboriosam excepturi pariatur expedita eum laudantium voluptatibus amet
+             </span>
+        </div>
+      </div>
+      <div className="bottom">
+          <div className="left">
+              <span className="logo">The Developer Shop</span>
+              <span className="copyright">
+                 @ Copyright 2023. All Rights Reserved
+              </span>
+          </div>
+          <div className="right">
+             <img src="/images/payment.png" alt="" />
+          </div>
+      </div>
+    </div>
   )
 }
 
